9ab94464d6b67cf03ec213bc0da90c468bc3be3f
Sergey Malashin <ser-malashin@yandex.com>
2018-04-16 12:55:31 +0300

    Добавьте файлы проекта.

---
+++ server/HeatDev/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeatDev.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+    }
+}
